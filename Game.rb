cfee8c25c270340786545ae947f001b0a2ccba0f
sz16900 <sz16900@my.bristol.ac.uk>
2020-02-13 12:50:27 -0500

    :sparkles: board class
    introducing board class with outcome logic

---
+++ Game.rb
@@ -0,0 +1,16 @@
+require "./Board"
+
+class Game
+    board = Board.new()
+
+    def start
+        while true
+            p "starting"
+        end
+    end
+end
+
+
+#start game 
+game = Game.new()
+game.start
